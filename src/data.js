b9326ec780b0db1ec4f81ebf8849ae8fd60c9d10
CyntiaDalesio <cyntiam.135@gmail.com>
2021-12-29 09:22:48 -0300

    first commit

--- src/data.js
+++ src/data.js
@@ -1,9 +1,43 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+
+export const sortByTitle = (data, OrderAsc) => {
+
+
+  if (OrderAsc) {
+    return data.sort(function (a, b) {
+      //al ser String ni number no puedo hacer un sort directo
+      //la funcion recibe un array de films
+      //tengo que hacer una funcion que me compare los titulos de dos films cada vez
+      if (a.title > b.title) {
+        return 1;
+      }
+      if (a.title < b.title) {
+        return -1;
+      }
+      return 0;
+    });
+  } else {
+    return data.sort(function (a, b) {
+      if (a.title > b.title) {
+        return -1;
+      }
+      if (a.title < b.title) {
+        return 1;
+      }
+      return 0;
+    });
+
+  }
+
+}
+
+export default sortByTitle;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,42 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
+import { sortByTitle } from './data.js';
+//almacena todos los films en una variable
+let films = data.films;
+//al cargar la pagina muestra los films desordenados
+showFilms(films);
+//mostrar los films ordenador por orden alfabetico
+let order = document.getElementById('order');
+order.addEventListener('click', function () {
+    let orderedFilms = sortByTitle(films, false);
+    showFilms(orderedFilms);
+});
+//recibe los fimls(filtrados u ordenados) y los muestra en pantalla creando sus respectivas etiquetas
+function showFilms(films) {
+    let seccionPoster = document.getElementById('pepito');
+    seccionPoster.innerHTML = ''; //sin esto, concatenaba otros films
+    for (let index = 0; index < films.length; index++) {
+        let film = films[index];
+        let poster = film.poster;
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('filmCard');
+        let elementTitle = document.createElement('p');
+        elementTitle.innerHTML = film.title;
+        let img = document.createElement('img');
+        img.src = poster;
+      newDiv.appendChild(elementTitle);
+      newDiv.appendChild(img);
+      //al elemento newElement le asigno dos elementos, un p y un img
+      seccionPoster.appendChild(newDiv);
+        // al elemento seccionPoster que es un div, le asigno el elemento new Element que consta de dos elementos (subnodos)
+    }
+};
 
-console.log(example, data);
+
+
+
+
+
+
+
+
+//console.log(example, data);

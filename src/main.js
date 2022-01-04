2c69b397fc42f017f4521e437856fa87e9bafe53
CyntiaDalesio <cyntiam.135@gmail.com>
2022-01-04 20:14:48 -0300

    cambios funcion sort

--- src/main.js
+++ src/main.js
@@ -19,21 +19,42 @@ let films = data.films;
 //al cargar la pagina muestra los films desordenados
 showFilms(films);
 //mostrar los films ordenador por orden alfabetico
-let order = document.getElementById('order');
-if (order) {
-    order.addEventListener('click', function () {
-        let orderedFilms = sortByTitle(films, false);
+
+let selectSortBy= document.getElementById('selectSortBy');
+if(selectSortBy){
+    let orderedFilms;
+    selectSortBy.addEventListener('change',function(){
+
+
+        switch (selectSortBy.value) {
+            case 'sortByAZ':
+             orderedFilms = sortByTitle(films, true); //orden ascendente true
+            
+                break;
+        
+            case 'sortByZA':
+               orderedFilms = sortByTitle(films, false); //orden ascendente false
+            
+                break;
+        }
+
         showFilms(orderedFilms);
-    });
-}
+      
+
+
+});}
+
+
+
+
 
 //recibe los fimls(filtrados u ordenados) y los muestra en pantalla creando sus respectivas etiquetas
 function showFilms(films) {
     let seccionPoster = document.getElementById('conteinerFilms');
     seccionPoster.innerHTML = ''; //sin esto, concatenaba otros films
     for (let index = 0; index < films.length; index++) {
-        let film = films[index];
-        let poster = film.poster;
+        let film = films[index]; //guardo el film actual
+        let poster = film.poster; // del film actual obtengo el poster
         let newDiv = document.createElement('div');
         newDiv.classList.add('filmCard');
         let elementTitle = document.createElement('p');
